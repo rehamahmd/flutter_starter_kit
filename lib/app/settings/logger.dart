0ee8465de3210b77c1f240e79b83c16c3ca15860
Reham-Ahmad <reham_ahmad94@yahoo.com>
2024-11-01 23:09:38 +0200

    add custom logging

--- lib/app/settings/logger.dart
+++ lib/app/settings/logger.dart
@@ -1,14 +1,14 @@
-// import 'package:flutter_starter_kit/app/imports.dart';
+import 'package:flutter_starter_kit/app/imports.dart';
 
-// void log(Object? message) => _customPrint(message, LogLevelEnum.debug);
-// void logInfo(Object? message) => _customPrint(message, LogLevelEnum.info);
-// void logWarning(Object? message) => _customPrint(message, LogLevelEnum.warning);
-// void logError(String? message, Object error) => _customPrint('$message ** $error', LogLevelEnum.error);
-// void logTrace(String? message, Object error, StackTrace stackTrace) =>
-//     _customPrint('$message ** $error\n, \x1B[1m[TRACE]\x1B[0m => \x1B[37m$stackTrace', LogLevelEnum.error);
+void log(Object? message) => _customPrint(message, LogLevelEnum.debug);
+void info(Object? message) => _customPrint(message, LogLevelEnum.info);
+void warning(Object? message) => _customPrint(message, LogLevelEnum.warning);
+void error(String? message, Object error) => _customPrint('$message ** $error', LogLevelEnum.error);
+void trace(String? message, Object error, StackTrace stackTrace) =>
+    _customPrint('$message ** $error\n, \x1B[1m[TRACE]\x1B[0m => \x1B[37m$stackTrace', LogLevelEnum.error);
 
-// void _customPrint(Object? message, LogLevelEnum level) {
-//   if (AppFlavors.appFlavors == Flavors.production) return;
-//   final currentTime = DateTimeUtils.timeFormat(DateTime.now());
-//   print('[${level.color}] $currentTime ${level.colorStart}=> $message${level.colorEnd} ');
-// }
+void _customPrint(Object? message, LogLevelEnum level) {
+  if (AppFlavors.appFlavors == Flavors.production) return;
+  final currentTime = DateTimeUtils.timeFormat(DateTime.now());
+  print('[${level.color}] $currentTime ${level.colorStart}=> $message${level.colorEnd} ');
+}
--- lib/src/common/enums/src/log_level_enum.dart
+++ lib/src/common/enums/src/log_level_enum.dart
@@ -1,5 +1,5 @@
 enum LogLevelEnum {
-  debug('DEBUG', '\x1B[1m\x1B[34mDEBUG\x1B[0m', '\x1B[34m', '\x1B[0m'),
+  debug('DEBUG', '\x1B[1m\x1B[32mDEBUG\x1B[0m', '\x1B[32m', '\x1B[0m'), // For Blue \x1B[34m
   info('INFO', '\x1B[1m\x1B[35mINFO\x1B[0m', '\x1B[35m', '\x1B[0m'),
   warning('WARNING', '\x1B[1m\x1B[33mWARNING\x1B[0m', '\x1B[33m', '\x1B[0m'),
   error('ERROR', '\x1B[1m\x1B[31mERROR\x1B[0m', '\x1B[31m', '\x1B[0m');
